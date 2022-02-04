271e06ee1a743075fb91b83484199c3c40885e34
Surender Kumar <56398576+s-suri@users.noreply.github.com>
2022-02-04 16:57:54 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:profilemachingapp/heart.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 import 'package:http/http.dart' as http;
 
@@ -34,7 +36,6 @@ class _MyAppState extends State<MyApp> {
 }
 
 class MyHomePage extends StatefulWidget {
-
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -79,7 +80,153 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: SalomonBottomBar(
+      backgroundColor: Color(0xFFF8BBD0),
+      bottomNavigationBar: bottomNavigationVar(),
+      body: screenBody(),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Container screenBody()
+  {
+    return Container(
+      color: Color(0xFFF8BBD0),
+      child: Stack(
+        children: [
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 120,
+                  width: MediaQuery.of(context).size.width/2.5,
+                  margin: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 3,
+                    ),
+                    image: DecorationImage(
+                        image: NetworkImage("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+                        fit: BoxFit.cover
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Container(
+                  margin: EdgeInsets.only(left: 10),
+                  alignment: Alignment.topLeft,
+                  child: Text("Uddesh Rajoriya",style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900
+                  ),
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Container(
+                  margin: EdgeInsets.only(left: 10),
+                  alignment: Alignment.topLeft,
+                  child: Text("Gwalior, India",style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w400
+                  ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      alignment: Alignment.topLeft,
+                      margin: EdgeInsets.only(left: 10),
+                      child: Text("Religion:",style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w400
+                      ),),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 10),
+                      child: Text("Hindu",style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.pink,
+                          fontWeight: FontWeight.w400
+                      ),),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10,),
+
+                Container(
+                  margin: EdgeInsets.only(left: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.pink,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  padding: EdgeInsets.only(left: 10,right: 10,top: 5,bottom: 5),
+
+                  child: Text("My Visitors",style: TextStyle(fontSize: 16,color: Colors.white),),
+                ),
+
+                SizedBox(height: 50,),
+              ],
+            ),
+          ), //Flexible
+
+          Container(
+            margin: EdgeInsets.only(top: 120),
+            alignment: Alignment.center,
+            child: Container(
+                width: 50,
+                height: MediaQuery.of(context).size.height,
+                child: HeartWidget()),//Container
+          ),
+
+
+          Container(
+            alignment: Alignment.bottomRight,
+            child: buildProductImageCarouselSlider(),//Container
+          ),
+
+
+
+          // Container(
+          //   margin: EdgeInsets.only(top: 100),
+          //   padding: EdgeInsets.only(bottom: 20),
+          //   alignment: Alignment.bottomCenter,
+          //   child: Padding(
+          //     padding: const EdgeInsets.all(8.0),
+          //     child: Row(
+          //       children: <Widget>[
+          //
+          //         Container(
+          //           alignment: Alignment.topCenter,
+          //           width: 40,
+          //           child: HeartWidget(),
+          //         ),
+          //
+          //
+          //       ], //<Widget>[]
+          //       mainAxisAlignment: MainAxisAlignment.center,
+          //     ),
+          //   ),
+          // ),
+        ],
+      ),
+    );
+  }
+
+  Container bottomNavigationVar()
+  {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
+
+      ),
+      child: SalomonBottomBar(
         currentIndex: _currentIndex,
         onTap: (i) {
           setState(() => _currentIndex = i);
@@ -108,16 +255,26 @@ class _MyHomePageState extends State<MyHomePage> {
           /// Search
           SalomonBottomBarItem(
             icon: Icon(Icons.mail_outline_sharp,color: _currentIndex == 2 ? Colors.pinkAccent : Colors.grey),
-            title: Text("Main",style:
+            title: Text("Mail",style:
             TextStyle(
                 color: Colors.pinkAccent
             ),),
             selectedColor: Colors.white,
           ),
 
-          /// Profile
           SalomonBottomBarItem(
-            icon: Icon(Icons.person,color: _currentIndex == 3 ? Colors.pinkAccent : Colors.grey),
+            icon: Container(
+              height: 30,
+              width: 30,
+              margin: EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                image: DecorationImage(
+                    image: NetworkImage("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+                    fit: BoxFit.cover
+                ),
+              ),
+            ),
             title: Text("Profile",style:
             TextStyle(
                 color: Colors.pinkAccent
@@ -126,106 +283,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Container(
-        color: Color(0xFFF06292),
-        child: Container(
-          margin: EdgeInsets.only(top: 100),
-          padding: EdgeInsets.only(bottom: 20),
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: <Widget>[
-                Flexible(
-                  flex: 1,
-                  fit: FlexFit.tight,
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 120,
-                        margin: EdgeInsets.only(left: 30),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 1,
-                          ),
-                          image: DecorationImage(
-                              image: NetworkImage("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
-                              fit: BoxFit.cover
-                          ),
-                        ),
-
-                      ),
-                      SizedBox(height: 10,),
-                      Container(
-                        margin: EdgeInsets.only(left: 30),
-                        alignment: Alignment.topLeft,
-                        child: Text("Uddesh Rajoriya",style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w900
-                        ),),
-                      ),
-                      SizedBox(height: 10,),
-                      Container(
-                        margin: EdgeInsets.only(left: 30),
-                        alignment: Alignment.topLeft,
-                        child: Text("Gwalior, India",style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.grey,
-                            fontWeight: FontWeight.w400
-                          ),
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Container(
-
-                            alignment: Alignment.topLeft,
-                            margin: EdgeInsets.only(left: 30),
-                            child: Text("Religion:",style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.grey,
-                                fontWeight: FontWeight.w400
-                            ),),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 10),
-                            child: Text("Hindu",style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.pink,
-                                fontWeight: FontWeight.w400
-                            ),),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10,),
-
-                      Container(
-                        padding: EdgeInsets.only(left: 10,right: 10,top: 5,bottom: 5),
-                        color: Colors.red,
-                        child: Text("My Visitors",style: TextStyle(fontSize: 16),),
-                      )
-
-                    ],
-                  ),//Container
-                ), //Flexible
-                SizedBox(
-                  width: 20,
-                ), //SizedBox
-                Flexible(
-                  flex: 1,
-                  fit: FlexFit.tight,
-                  child: buildProductImageCarouselSlider(),//Container
-                ),
-              ], //<Widget>[]
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ),
-        ),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
@@ -237,93 +294,115 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     } else {
       return Container(
-        child: Column(
-          children: [
-            Container(
+        height: 300,
+        margin: EdgeInsets.only(right: 10),
+        alignment: Alignment.bottomRight,
+        child: CarouselSlider(
+          carouselController: _imageCarouselController,
+          options: CarouselOptions(
+              aspectRatio: 0.5,
+              viewportFraction: 1,
+              initialPage: 0,
+              enableInfiniteScroll: false,
+              reverse: false,
+              autoPlay: false,
+              scrollDirection: Axis.vertical,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentImage = index;
+                });
+              }),
+          items: _carouselImageList.map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      alignment: Alignment.topLeft,
+                      height: 130,
+                      width: MediaQuery.of(context).size.width/2.5,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 3,
+                        ),
+                        image: DecorationImage(
+                            image: NetworkImage("https://i.pinimg.com/564x/1e/88/93/1e889313773c6f02fe21a878d73d437f.jpg"),
+                            fit: BoxFit.cover
+                        ),
+                      ),
+                    ),
 
-              child: CarouselSlider(
-                carouselController: _imageCarouselController,
-                options: CarouselOptions(
-                    aspectRatio: 1,
-                    viewportFraction: 1,
-                    initialPage: 0,
-                    enableInfiniteScroll: false,
-                    reverse: false,
-                    autoPlay: false,
-                    scrollDirection: Axis.vertical,
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        _currentImage = index;
-                      });
-                    }),
-                items: _carouselImageList.map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
+                    SizedBox(height: 5,),
+                    Container(
+                      width: MediaQuery.of(context).size.width/2.5,
+                      alignment: Alignment.topLeft,
+                      child: Text("${_carouselImageList[_currentImage]['name']}",style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w900
+                      ),),
+                    ),
+                    SizedBox(height: 5,),
+                    Container(
+                      width: MediaQuery.of(context).size.width/2.5,
+                      alignment: Alignment.topLeft,
+                      child: Text("${_carouselImageList[_currentImage]['status']}",style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w400
+                      ),
+                      ),
+                    ),
+
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
                           decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            border: Border.all(
-                              color: Colors.black,
-                              width: 1,
-                            ),
+                              color: Colors.pink,
+                              borderRadius: BorderRadius.circular(10)
                           ),
-                          child: FadeInImage.assetNetwork(
-                            placeholder: 'assets/placeholder.gif',
-                            image: "https://i.pinimg.com/564x/1e/88/93/1e889313773c6f02fe21a878d73d437f.jpg",
-                            fit: BoxFit.cover,
-                          )
-                      );
-                    },
-                  );
-                }).toList(),
-              ),
-            ),
-            Container(
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(left: 30,top: 20),
-                    alignment: Alignment.topLeft,
-                    child: Text("${_carouselImageList[_currentImage]['name']}",style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                        fontWeight: FontWeight.w900
-                    ),),
-                  ),
-                  SizedBox(height: 10,),
-                  Container(
-                    margin: EdgeInsets.only(left: 30),
-                    alignment: Alignment.topLeft,
-                    child: Text("${_carouselImageList[_currentImage]['status']}",style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w400
+                          padding: EdgeInsets.only(left: 5,right: 5,top: 5,bottom: 5),
+                          margin: EdgeInsets.only(left: 5,right: 5,top: 5,bottom: 5),
+                          child: Icon(Icons.wallet_membership_rounded,color: Colors.white,),
+                        ),
+
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.pink,
+                              borderRadius: BorderRadius.circular(10)
+                          ),
+                          padding: EdgeInsets.only(left: 5,right: 5,top: 5,bottom: 5),
+                          margin: EdgeInsets.only(left: 5,right: 5,top: 5,bottom: 5),
+
+                          child: Icon(Icons.message_outlined,color: Colors.white,),
+                        ),
+
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.pink,
+                              borderRadius: BorderRadius.circular(10)
+                          ),
+                          padding: EdgeInsets.only(left: 5,right: 5,top: 5,bottom: 5),
+                          margin: EdgeInsets.only(left: 5,right: 5,top: 5,bottom: 5),
+                          child: Icon(Icons.arrow_downward,color: Colors.white,),
+                        ),
+                      ],
                     ),
-                    ),
-                  ),
-                  SizedBox(height: 10,),
 
-                  Row(
-                    children: [
-                      SizedBox(width: 20,),
-                      Container(
-                        padding: EdgeInsets.only(left: 10,right: 10,top: 5,bottom: 5),
-                        color: Colors.red,
-                        child: Icon(Icons.message_outlined,color: Colors.white,),
-                      ),
+                    SizedBox(height: 30,),
 
-                      Container(
-                        padding: EdgeInsets.only(left: 10,right: 10,top: 5,bottom: 5),
-                        color: Colors.red,
-                        child: Icon(Icons.arrow_downward,color: Colors.white,),
-                      ),
-
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ],
+                  ],
+                );
+              },
+            );
+          }).toList(),
         ),
       );
     }
